02a1277bc31129b441fbae44d61b4c0403a4c58a
jaanisfehling <jaanisfehling@gmail.com>
2023-03-31 13:16:25 +0200

    remove unused import

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 
-import { WebSocket, WebSocketServer } from 'ws';
+import { WebSocketServer } from 'ws';
 import {saveArticle} from "./db.js";
 const {Worker} = require("worker_threads");
 
 
 const receiver = new WebSocketServer({ port: 8888 })
 console.log("Opened websocket at Port 8888");
-const sender = new WebSocket("ws://localhost:8000/ws/news");
+// const sender = new WebSocket("ws://localhost:8000/ws/news");
 
 
 receiver.on("connection", ws => {
@@ -16,6 +16,7 @@ receiver.on("connection", ws => {
 
     ws.on("message", async data => {
         console.log("Received article");
+        console.log(data);
         let article = JSON.parse(data);
         const worker = new Worker("./worker.js", {
             workerData: {
@@ -24,7 +25,8 @@ receiver.on("connection", ws => {
         });
         worker.once("message", async result => {
             await saveArticle(result);
-            sender.send(JSON.stringify(result));
+            console.log("Sending Result...");
+            // sender.send(JSON.stringify(result));
         });
         worker.on("error", (error) => {
             console.log("Error in Worker for " + article.url + error);
